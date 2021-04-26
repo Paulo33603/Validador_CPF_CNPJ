e042d767c4b69ab4a7c0c29ffd6d332996132fad
Paulo Fagundes de Campos <fagundespaulo.ph@gmail.com>
2021-04-26 12:32:34 -0300

    first commit

---
+++ ValidaCPFeCNPJ/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace ValidaCPFeCNPJ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboEstados = new System.Windows.Forms.ComboBox();
+            this.txtDocumentoGerado = new System.Windows.Forms.TextBox();
+            this.radioGerarCPF = new System.Windows.Forms.RadioButton();
+            this.radioGerarCNPJ = new System.Windows.Forms.RadioButton();
+            this.BtnGerador = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.radioTitular = new System.Windows.Forms.RadioButton();
+            this.RadioCNPJ = new System.Windows.Forms.RadioButton();
+            this.RadioCPF = new System.Windows.Forms.RadioButton();
+            this.btnVerifica = new System.Windows.Forms.Button();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(256, 183);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.comboEstados);
+            this.tabPage1.Controls.Add(this.txtDocumentoGerado);
+            this.tabPage1.Controls.Add(this.radioGerarCPF);
+            this.tabPage1.Controls.Add(this.radioGerarCNPJ);
+            this.tabPage1.Controls.Add(this.BtnGerador);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(248, 157);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Gerador";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Escolha um estado para o CPF";
+            // 
+            // comboEstados
+            // 
+            this.comboEstados.FormattingEnabled = true;
+            this.comboEstados.Items.AddRange(new object[] {
+            "AC",
+            "AL",
+            "AP",
+            "AM",
+            "BA",
+            "CE",
+            "DF",
+            "ES",
+            "GO",
+            "MA",
+            "MT",
+            "MS",
+            "MG",
+            "PA",
+            "PB",
+            "PR",
+            "PE",
+            "PI",
+            "RJ",
+            "RN",
+            "RS",
+            "RO",
+            "RR",
+            "SC",
+            "SP",
+            "TE",
+            "TO"});
+            this.comboEstados.Location = new System.Drawing.Point(68, 74);
+            this.comboEstados.Name = "comboEstados";
+            this.comboEstados.Size = new System.Drawing.Size(100, 21);
+            this.comboEstados.TabIndex = 11;
+            // 
+            // txtDocumentoGerado
+            // 
+            this.txtDocumentoGerado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDocumentoGerado.Location = new System.Drawing.Point(43, 129);
+            this.txtDocumentoGerado.Name = "txtDocumentoGerado";
+            this.txtDocumentoGerado.Size = new System.Drawing.Size(167, 22);
+            this.txtDocumentoGerado.TabIndex = 10;
+            // 
+            // radioGerarCPF
+            // 
+            this.radioGerarCPF.AutoSize = true;
+            this.radioGerarCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioGerarCPF.Location = new System.Drawing.Point(56, 12);
+            this.radioGerarCPF.Name = "radioGerarCPF";
+            this.radioGerarCPF.Size = new System.Drawing.Size(52, 20);
+            this.radioGerarCPF.TabIndex = 8;
+            this.radioGerarCPF.TabStop = true;
+            this.radioGerarCPF.Text = "CPF";
+            this.radioGerarCPF.UseVisualStyleBackColor = true;
+            // 
+            // radioGerarCNPJ
+            // 
+            this.radioGerarCNPJ.AutoSize = true;
+            this.radioGerarCNPJ.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioGerarCNPJ.Location = new System.Drawing.Point(120, 12);
+            this.radioGerarCNPJ.Name = "radioGerarCNPJ";
+            this.radioGerarCNPJ.Size = new System.Drawing.Size(61, 20);
+            this.radioGerarCNPJ.TabIndex = 9;
+            this.radioGerarCNPJ.TabStop = true;
+            this.radioGerarCNPJ.Text = "CNPJ";
+            this.radioGerarCNPJ.UseVisualStyleBackColor = true;
+            // 
+            // btnGerador
+            // 
+            this.BtnGerador.Location = new System.Drawing.Point(68, 101);
+            this.BtnGerador.Name = "BtnGerador";
+            this.BtnGerador.Size = new System.Drawing.Size(100, 22);
+            this.BtnGerador.TabIndex = 7;
+            this.BtnGerador.Text = "Gerar";
+            this.BtnGerador.UseVisualStyleBackColor = true;
+            this.BtnGerador.Click += new System.EventHandler(this.BtnGerador_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.radioTitular);
+            this.tabPage2.Controls.Add(this.RadioCNPJ);
+            this.tabPage2.Controls.Add(this.RadioCPF);
+            this.tabPage2.Controls.Add(this.btnVerifica);
+            this.tabPage2.Controls.Add(this.txtDocumento);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(248, 157);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Validador";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // radioTitular
+            // 
+            this.radioTitular.AutoSize = true;
+            this.radioTitular.Location = new System.Drawing.Point(146, 76);
+            this.radioTitular.Name = "radioTitular";
+            this.radioTitular.Size = new System.Drawing.Size(54, 17);
+            this.radioTitular.TabIndex = 11;
+            this.radioTitular.TabStop = true;
+            this.radioTitular.Text = "Titular";
+            this.radioTitular.UseVisualStyleBackColor = true;
+            // 
+            // RadioCNPJ
+            // 
+            this.RadioCNPJ.AutoSize = true;
+            this.RadioCNPJ.Location = new System.Drawing.Point(88, 76);
+            this.RadioCNPJ.Name = "RadioCNPJ";
+            this.RadioCNPJ.Size = new System.Drawing.Size(52, 17);
+            this.RadioCNPJ.TabIndex = 10;
+            this.RadioCNPJ.TabStop = true;
+            this.RadioCNPJ.Text = "CNPJ";
+            this.RadioCNPJ.UseVisualStyleBackColor = true;
+            // 
+            // RadioCPF
+            // 
+            this.RadioCPF.AutoSize = true;
+            this.RadioCPF.Location = new System.Drawing.Point(37, 76);
+            this.RadioCPF.Name = "RadioCPF";
+            this.RadioCPF.Size = new System.Drawing.Size(45, 17);
+            this.RadioCPF.TabIndex = 9;
+            this.RadioCPF.TabStop = true;
+            this.RadioCPF.Text = "CPF";
+            this.RadioCPF.UseVisualStyleBackColor = true;
+            // 
+            // btnVerifica
+            // 
+            this.btnVerifica.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnVerifica.Location = new System.Drawing.Point(130, 106);
+            this.btnVerifica.Name = "btnVerifica";
+            this.btnVerifica.Size = new System.Drawing.Size(85, 28);
+            this.btnVerifica.TabIndex = 0;
+            this.btnVerifica.Text = "Verificar";
+            this.btnVerifica.UseVisualStyleBackColor = true;
+            this.btnVerifica.Click += new System.EventHandler(this.BtnVerifica_Click);
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Location = new System.Drawing.Point(37, 50);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(150, 20);
+            this.txtDocumento.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Digite seu CPF ou CNPJ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(277, 202);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Valida CPF/CNPJ";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RadioButton RadioCNPJ;
+        private System.Windows.Forms.Button btnVerifica;
+        private System.Windows.Forms.RadioButton RadioCPF;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDocumentoGerado;
+        private System.Windows.Forms.RadioButton radioGerarCPF;
+        private System.Windows.Forms.RadioButton radioGerarCNPJ;
+        private System.Windows.Forms.Button BtnGerador;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboEstados;
+        private System.Windows.Forms.RadioButton radioTitular;
+    }
+}
+
---
+++ ValidaCPFeCNPJ/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidaCPFeCNPJ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnVerifica_Click(object sender, EventArgs e)
+        {
+            string caracteres = txtDocumento.Text;
+
+            caracteres = caracteres.Replace(".", String.Empty).Replace("/", String.Empty).Replace("-", String.Empty);
+            
+            if (caracteres.Length == 11 && RadioCPF.Checked)
+            {
+                string cpf = caracteres;
+                
+                int digito1 = int.Parse(cpf.Substring(0, 1));
+                int digito2 = int.Parse(cpf.Substring(1, 1));
+                int digito3 = int.Parse(cpf.Substring(2, 1));
+                int digito4 = int.Parse(cpf.Substring(3, 1));
+                int digito5 = int.Parse(cpf.Substring(4, 1));
+                int digito6 = int.Parse(cpf.Substring(5, 1));
+                int digito7 = int.Parse(cpf.Substring(6, 1));
+                int digito8 = int.Parse(cpf.Substring(7, 1));
+                int digito9 = int.Parse(cpf.Substring(8, 1));
+                int digito10 = int.Parse(cpf.Substring(9, 1));
+                int digito11 = int.Parse(cpf.Substring(10, 1));
+
+                int total = digito1 * 10 + digito2 * 9 + digito3 * 8 + digito4 * 7 + digito5 * 6 + digito6 * 5 + digito7 * 4 + digito8 * 3 + digito9 * 2;
+
+                int resto = total % 11;
+
+                int verificado1;
+
+                if (resto == 0 || resto == 1)
+                {
+                    verificado1 = 0;
+                }
+                else
+                {
+                    verificado1 = 11 - resto;
+                }
+
+                total = digito1 * 11 + digito2 * 10 + digito3 * 9 + digito4 * 8 + digito5 * 7 + digito6 * 6 + digito7 * 5 + digito8 * 4 + digito9 * 3 + digito10 * 2;
+
+                resto = total % 11;
+
+                int verificado2;
+
+                if (resto == 0 || resto == 1)
+                {
+                    verificado2 = 0;
+                }
+                else
+                {
+                    verificado2 = 11 - resto;
+                }
+
+                if (verificado1 == digito10 && verificado2 == digito11)
+                {
+                    MessageBox.Show("O CPF " + digito1 + digito2 + digito3 + "." + digito4 + digito5 + digito6 + "." + digito7 + digito8 + digito9 + "-" + digito10 + digito11 + " é válido");
+                }
+                else
+                {
+                    MessageBox.Show("O CPF não é válido");
+                }
+            }
+            else if (caracteres.Length == 14 && RadioCNPJ.Checked)
+            {
+                string cnpj = Convert.ToString(caracteres);
+
+                int digito1 = int.Parse(cnpj.Substring(0, 1));
+                int digito2 = int.Parse(cnpj.Substring(1, 1));
+                int digito3 = int.Parse(cnpj.Substring(2, 1));
+                int digito4 = int.Parse(cnpj.Substring(3, 1));
+                int digito5 = int.Parse(cnpj.Substring(4, 1));
+                int digito6 = int.Parse(cnpj.Substring(5, 1));
+                int digito7 = int.Parse(cnpj.Substring(6, 1));
+                int digito8 = int.Parse(cnpj.Substring(7, 1));
+                int digito9 = int.Parse(cnpj.Substring(8, 1));
+                int digito10 = int.Parse(cnpj.Substring(9, 1));
+                int digito11 = int.Parse(cnpj.Substring(10, 1));
+                int digito12 = int.Parse(cnpj.Substring(11, 1));
+                int digito13 = int.Parse(cnpj.Substring(12, 1));
+                int digito14 = int.Parse(cnpj.Substring(13, 1));
+
+                int total = digito1 * 5 + digito2 * 4 + digito3 * 3 + digito4 * 2 + digito5 * 9 + digito6 * 8 + digito7 * 7 + digito8 * 6 + digito9 * 5 + digito10 * 4 + digito11 * 3 + digito12 * 2;
+
+                int resto = total % 11;
+
+                int verificador1;
+
+                if (resto < 2)
+                {
+                    verificador1 = 0;
+                }
+                else
+                {
+                    verificador1 = 11 - resto;
+                }
+
+                total = digito1 * 6 + digito2 * 5 + digito3 * 4 + digito4 * 3 + digito5 * 2 + digito6 * 9 + digito7 * 8 + digito8 * 7 + digito9 * 6 + digito10 * 5 + digito11 * 4 + digito12 * 3 + digito13 * 2;
+
+                resto = total % 11;
+
+                int verificador2;
+
+                if (resto < 2)
+                {
+                    verificador2 = 0;
+                }
+                else
+                {
+                    verificador2 = 11 - resto;
+                }
+
+                if (verificador1 == digito13 && verificador2 == digito14)
+                {
+                    MessageBox.Show("O CNPJ " + digito1 + digito2 + "." + digito3 + digito4 + digito5 + "." + digito6 + digito7 + digito8 + "/" + digito9 + digito10 + digito11 + digito12 + "-" + digito13 + digito14 + " é válido");
+                }
+                else
+                {
+                    MessageBox.Show("O CNPJ não existe");
+                }
+            }
+            else
+            {
+                MessageBox.Show("O formato não é válido ou o documento não foi selecionado");
+            }
+        }
+
+        private void BtnGerador_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            
+            if (radioGerarCPF.Checked)
+            {
+                int digito1 = rnd.Next(10);
+                int digito2 = rnd.Next(10);
+                int digito3 = rnd.Next(10);
+                int digito4 = rnd.Next(10);
+                int digito5 = rnd.Next(10);
+                int digito6 = rnd.Next(10);
+                int digito7 = rnd.Next(10);
+                int digito8 = rnd.Next(10);
+                int digito9 = rnd.Next(10);
+
+                if (comboEstados.Text == "RS")
+                {
+                    digito9 = 0;
+                }
+                else if (comboEstados.Text == "DF" || comboEstados.Text == "GO" || comboEstados.Text == "MG" || comboEstados.Text == "MS" || comboEstados.Text == "TO")
+                {
+                    digito9 = 1;
+                }
+                else if (comboEstados.Text == "AM" || comboEstados.Text == "PA" || comboEstados.Text == "RR" || comboEstados.Text == "AP" || comboEstados.Text == "AC" || comboEstados.Text == "RO")
+                {
+                    digito9 = 2;
+                }
+                else if (comboEstados.Text == "CE" || comboEstados.Text == "MA" || comboEstados.Text == "PI")
+                {
+                    digito9 = 3;
+                }
+                else if (comboEstados.Text == "PB" || comboEstados.Text == "PE" || comboEstados.Text == "AL" || comboEstados.Text == "RN")
+                {
+                    digito9 = 4;
+                }
+                else if (comboEstados.Text == "BA" || comboEstados.Text == "SE")
+                {
+                    digito9 = 5;
+                }
+                else if (comboEstados.Text == "MG")
+                {
+                    digito9 = 6;
+                }
+                else if (comboEstados.Text == "RJ" || comboEstados.Text == "ES")
+                {
+                    digito9 = 7;
+                }
+                else if (comboEstados.Text == "SP")
+                {
+                    digito9 = 8;
+                }
+                else if(comboEstados.Text == "PR" || comboEstados.Text == "SC")
+                {
+                    digito9 = 9;
+                }
+
+                int total = digito1 * 10 + digito2 * 9 + digito3 * 8 + digito4 * 7 + digito5 * 6 + digito6 * 5 + digito7 * 4 + digito8 * 3 + digito9 * 2;
+
+                int resto = total % 11;
+
+                int verificado1;
+
+                if (resto == 0 || resto == 1)
+                {
+                    verificado1 = 0;
+                }
+                else
+                {
+                    verificado1 = 11 - resto;
+                }
+
+                total = digito1 * 11 + digito2 * 10 + digito3 * 9 + digito4 * 8 + digito5 * 7 + digito6 * 6 + digito7 * 5 + digito8 * 4 + digito9 * 3 + verificado1 * 2;
+
+                resto = total % 11;
+
+                int verificado2;
+
+                if (resto == 0 || resto == 1)
+                {
+                    verificado2 = 0;
+                }
+                else
+                {
+                    verificado2 = 11 - resto;
+                }
+
+                txtDocumentoGerado.Text = $"{digito1}{digito2}{digito3}.{digito4}{digito5}{digito6}.{digito7}{digito8}{digito9}-{verificado1}{verificado2}";
+            }
+            else if (radioGerarCNPJ.Checked)
+            {
+                int digito1 = rnd.Next(10);
+                int digito2 = rnd.Next(10);
+                int digito3 = rnd.Next(10);
+                int digito4 = rnd.Next(10);
+                int digito5 = rnd.Next(10);
+                int digito6 = rnd.Next(10);
+                int digito7 = rnd.Next(10);
+                int digito8 = rnd.Next(10);
+                int digito9 = rnd.Next(10);
+                int digito10 = rnd.Next(10);
+                int digito11 = rnd.Next(10);
+                int digito12 = rnd.Next(10);
+
+                int total = digito1 * 5 + digito2 * 4 + digito3 * 3 + digito4 * 2 + digito5 * 9 + digito6 * 8 + digito7 * 7 + digito8 * 6 + digito9 * 5 + digito10 * 4 + digito11 * 3 + digito12 * 2;
+
+                int resto = total % 11;
+
+                int verificador1;
+
+                if (resto < 2)
+                {
+                    verificador1 = 0;
+                }
+                else
+                {
+                    verificador1 = 11 - resto;
+                }
+
+                total = digito1 * 6 + digito2 * 5 + digito3 * 4 + digito4 * 3 + digito5 * 2 + digito6 * 9 + digito7 * 8 + digito8 * 7 + digito9 * 6 + digito10 * 5 + digito11 * 4 + digito12 * 3 + verificador1 * 2;
+
+                resto = total % 11;
+
+                int verificador2;
+
+                if (resto >= 2)
+                {
+                    verificador2 = 11 - resto;
+                }
+                else
+                {
+                    verificador2 = 0;
+                }
+
+                txtDocumentoGerado.Text = $"{digito1}{digito2}.{digito3}{digito4}{digito5}.{digito6}{digito7}{digito8}/{digito9}{digito10}{digito11}{digito12}-{verificador1}{verificador2}";
+            }
+        }
+    }
+}
